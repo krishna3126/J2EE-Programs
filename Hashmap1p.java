901be3c9013ce9bd24ed36c5fcec95591c235705
Krishna  Lahade <lahadekrishna8767@gmail.com>
2026-01-31 10:44:51 +0530

    Add files via upload

---
+++ Hashmap1p.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class Hashmap1p
+ {
+  public static void main(String args[])
+  {
+  
+    HashMap<Integer, String> hash = new HashMap <>();
+	
+    hash.put(1001, "Pavan");
+    hash.put(1002, "Yash");
+    hash.put(1003, "Vijay");
+    hash.put(1004, "Ganesh");
+    hash.put(1005, "Sagar");
+	
+    System.out.println(hash);
+	}
+	}
